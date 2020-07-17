1b36431b16ab98b012edffbc9bfcb96d17cdba62
richardbatstone <36573392+richardbatstone@users.noreply.github.com>
2020-07-17 16:32:47 +0100

    Make all one column

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import {FlexGrid, FlexGridItem} from 'baseui/flex-grid'
-import {Paragraph3, Label3} from 'baseui/typography'
+import {Paragraph3, Label3, H3} from 'baseui/typography'
 
 import NAPlogo from './notaproduct2.png'
 import Introduction from './Introduction'
 import SliderInput from './SliderInput'
 import DragVisual from './DragVisual'
-import Discussion from './Discussion'
 import Footer from './Footer'
 
 
@@ -39,47 +38,57 @@ export default function MainContent() {
         <Introduction />
       </FlexGridItem>
       <FlexGridItem {...itemProps}>
-        <FlexGrid
-          flexGridColumnCount={2}
-          alignSelf="center"
-          alignItems="center"
-          width="100%">
-          <FlexGridItem {...itemProps}>
-            <Label3>PE Fund's Holding</Label3>
-            <Paragraph3>         
-               Use the slider to set the size (%) of the PE Fund's stake. For simplicity, 
-               all other shareholders are lumped together as 'other' and we ignore things 
-               like A and B shares.
-          </Paragraph3>
-          </FlexGridItem>
+        <Label3>PE Fund's Holding</Label3>
+        <Paragraph3>         
+            Use the slider to set the size (%) of the PE Fund's stake. For simplicity, 
+            all other shareholders are lumped together as 'other' and we ignore things 
+            like A and B shares.
+      </Paragraph3>
+      </FlexGridItem>
           <FlexGridItem {...itemProps}>
             <SliderInput sliderName="propHolding" data={sliderValue.propHolding} setSliderValue={setSliderValue} />
           </FlexGridItem>
-        </FlexGrid>
+      <FlexGridItem {...itemProps}>
+        <Label3>Drag trigger</Label3>
+        <Paragraph3>         
+          Use the slider to set the proportion of shares that the Fund would need to sell 
+          before its 'drag rights' apply.
+      </Paragraph3>
       </FlexGridItem>
       <FlexGridItem {...itemProps}>
-        <FlexGrid
-          flexGridColumnCount={2}
-          alignSelf="center"
-          alignItems="center"
-          width="100%">
-          <FlexGridItem {...itemProps}>
-            <Label3>Drag trigger</Label3>
-            <Paragraph3>         
-              Use the slider to set the proportion of shares that the Fund would need to sell 
-              before its 'drag rights' apply.
-          </Paragraph3>
-          </FlexGridItem>
-          <FlexGridItem {...itemProps}>
-            <SliderInput sliderName="dragTrigger" data={sliderValue.dragTrigger} setSliderValue={setSliderValue} />
-          </FlexGridItem>
-        </FlexGrid>   
+        <SliderInput sliderName="dragTrigger" data={sliderValue.dragTrigger} setSliderValue={setSliderValue} />
       </FlexGridItem>
       <FlexGridItem>
         <DragVisual data={sliderValue} setSliderValue={setSliderValue}/>
       </FlexGridItem>
-      <FlexGridItem>
-        <Discussion />
+      <FlexGridItem {...itemProps}>
+      <H3>Yes and...</H3>
+        <Paragraph3>
+          You could:
+          <ul>
+              <li>Create a whole 'exit provisions' pack to inform deal sign-off</li>
+              <li>Hook it up to your doc auto system so you get the visuals automatically</li>
+              <li>Hook it up to your smart contract system to create a little legal dashboard</li>
+              <li>Record the meta data using Akoma Ntoso</li>
+              <li>Embed the meta data in a QR code so it is preserved on conversion to pdf</li>
+          </ul>
+        </Paragraph3>
+        </FlexGridItem>
+      <FlexGridItem {...itemProps}>
+      <H3>Yes but...</H3>
+        <Paragraph3>
+            Hang on:
+            <ul>
+                <li>What's the actual user need that this is meeting? Will different circumstances be similar enough
+                    to be addressed with a template?
+                </li>
+                <li>What happens when people start manipulating the image in paint?</li>
+                <li>What happens when people export their Word doc to pdf?</li>
+                <li>There's not enough consistency in the way meta data is parsed by different systems to make recording
+                    semantic meta data viable.
+                </li>
+            </ul>
+        </Paragraph3>
       </FlexGridItem>
       <FlexGridItem backgroundColor= 'mono300' display="flex" justifyContent="center">
         <Footer />
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider as StyletronProvider} from 'styletron-react';
 import {Client as Styletron} from 'styletron-engine-atomic';
-import {LightTheme, DarkTheme, BaseProvider} from 'baseui';
+import {LightTheme, BaseProvider} from 'baseui';
 
 const engine = new Styletron();
 ReactDOM.render(
